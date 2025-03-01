7b5b94539255d2a4d12ac85385d08756f7d49fb7
Hayat ilyas <ihayat855@gmail.com>
2025-03-01 18:38:17 +0530

    Changed the logo name

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -6,73 +6,73 @@ import Link from "next/link";
 import { siteConfig } from "@/lib/siteConfig";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
 });
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  metadataBase: new URL(siteConfig.url),
-  description: siteConfig.description,
-  keywords: siteConfig.keywords,
-  authors: [
-    {
-      name: "Hayat Ilyas",
-      url: "https://hayat-beta.vercel.app",
+    title: {
+        default: siteConfig.name,
+        template: `%s - ${siteConfig.name}`,
     },
-  ],
-  creator: "Hayat ilyas",
-  openGraph: {
-    type: "website",
-    locale: "en_IN",
-    url: siteConfig.url,
-    title: siteConfig.name,
+    metadataBase: new URL(siteConfig.url),
     description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
+    keywords: siteConfig.keywords,
+    authors: [
+        {
+            name: "Hayat Ilyas",
+            url: "https://hayat-beta.vercel.app",
+        },
     ],
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
+    creator: "Hayat ilyas",
+    openGraph: {
+        type: "website",
+        locale: "en_IN",
+        url: siteConfig.url,
+        title: siteConfig.name,
+        description: siteConfig.description,
+        siteName: siteConfig.name,
+        images: [
+            {
+                url: siteConfig.ogImage,
+                width: 1200,
+                height: 630,
+                alt: siteConfig.name,
+            },
+        ],
+    },
+    icons: {
+        icon: "/favicon.ico",
+        shortcut: "/favicon-16x16.png",
+        apple: "/apple-touch-icon.png",
+    },
+    manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <header className="border-b sticky top-0 bg-background z-50">
-          <div className="container mx-auto px-4 py-4">
-            <div className="flex items-center justify-between">
-              <h1 className="text-2xl font-bold">
-                <Link href="/">NewsDaily CMS</Link>{" "}
-              </h1>
-              <Navbar />
-            </div>
-          </div>
-        </header>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+                <header className="border-b sticky top-0 bg-background z-50">
+                    <div className="container mx-auto px-4 py-4">
+                        <div className="flex items-center justify-between">
+                            <h1 className="text-2xl font-bold">
+                                <Link href="/">Technology CMS</Link>{" "}
+                            </h1>
+                            <Navbar />
+                        </div>
+                    </div>
+                </header>
+                {children}
+            </body>
+        </html>
+    );
 }
